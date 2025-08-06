d2c678844e855cd05e721d111123191f902e9ce3
Mikel Garín <mikelgarin@gmail.com>
2025-08-06 21:54:09 +0200

    Styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,20 @@ export default function Navbar() {
   const { user } = useContext(AuthContext);
 
   return (
-    <div className="relative">
-      <nav className="bg-emerald-950 text-white px-6 py-4 flex items-center justify-between shadow-lg z-10 relative">
-        <Link to="/" className="flex items-center gap-3">
+    <div className="relative bg-emerald-950 shadow-lg z-10">
+      <nav className="px-6 md:container md:mx-auto md:px-8 py-4 flex items-center justify-between">
+        <Link to="/" className="flex items-center gap-2 md:gap-3">
           <img
             src="/ranky-logo.png"
             alt="Ranky logo"
-            className="w-10 h-10 rounded-full"
+            className="w-8 h-8 md:w-10 md:h-10 rounded-full"
           />
-          <span className="text-3xl font-bold bg-gradient-to-r from-yellow-400 via-yellow-300 to-gray-300 text-transparent bg-clip-text">
+          <span className="text-xl md:text-3xl font-bold md:font-black tracking-tight bg-gradient-to-r from-yellow-400 via-yellow-300 to-gray-300 text-transparent bg-clip-text">
             Ranky
           </span>
         </Link>
 
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-3 md:gap-4">
           {user && <Link to="/" className="hover:underline">Home</Link>}
           {user && <a href="https://api.ranky.top/auth/logout" className="hover:underline">Log out</a>}
 
@@ -27,14 +27,15 @@ export default function Navbar() {
             href="https://discord.com/oauth2/authorize?client_id=1005188427634966629&permissions=2952866832&scope=bot"
             target="_blank"
             rel="noopener noreferrer"
-            className="bg-white text-green-800 font-semibold px-4 py-2 rounded hover:bg-green-100 transition"
+            className="bg-gradient-to-r from-yellow-200 via-yellow-100 to-yellow-50 text-amber-900 font-semibold px-3 md:px-4 py-2 rounded hover:brightness-105 transition text-sm md:text-base"
           >
             Add Ranky to your Discord server
           </a>
         </div>
       </nav>
 
-      <div className="absolute bottom-0 left-0 right-0 h-4 bg-gradient-to-b from-emerald-950 to-transparent pointer-events-none z-0" />
+      {/* Fade visual inferior */}
+      <div className="absolute bottom-0 left-0 right-0 h-4 bg-gradient-to-b from-emerald-950 to-transparent pointer-events-none" />
     </div>
   );
 }
