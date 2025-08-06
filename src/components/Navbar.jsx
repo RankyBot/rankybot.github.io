3c83ed6e7a72b7e3bbb5eab333fbc24b7e4cc1c8
Mikel Garín <mikelgarin@gmail.com>
2025-08-06 22:55:01 +0200

    Styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,20 @@ export default function Navbar() {
   const { user } = useContext(AuthContext);
 
   return (
-    <div className="relative bg-emerald-950 shadow-lg z-10 bg-ranky-texture bg-[length:80px] bg-repeat opacity-10 z-10">
-          <nav className="px-6 md:container md:mx-auto md:px-8 py-4 flex items-center justify-between">
+    <div className="relative bg-emerald-950 shadow-lg z-10 bg-ranky-texture bg-[length:80px] bg-repeat opacity-10 z-10 shadow-xl">
+      <nav className="px-6 md:container md:mx-auto md:px-8 py-4 flex items-center justify-between">
         <Link to="/" className="flex items-center gap-2 md:gap-3">
           <img
             src="/ranky-logo.png"
             alt="Ranky logo"
-            className="w-8 h-8 md:w-10 md:h-10 rounded-full"
+            className="w-8 h-8 md:w-10 md:h-10 rounded-full opacity-100"
           />
-          <span className="text-xl md:text-3xl font-black tracking-tight bg-gradient-to-r from-yellow-400 via-yellow-300 to-gray-300 text-transparent bg-clip-text">
+          <span className="text-xl opacity-100 md:text-3xl font-black tracking-tight bg-gradient-to-r from-yellow-400 via-yellow-300 to-gray-300 text-transparent bg-clip-text">
             Ranky
           </span>
         </Link>
 
-        <div className="flex items-center gap-3 md:gap-4">
+        <div className="flex items-center gap-3 md:gap-4 opacity-100">
           {user && <Link to="/" className="hover:underline">Home</Link>}
           {user && <a href="https://api.ranky.top/auth/logout" className="hover:underline">Log out</a>}
 
