530385b15a17edbebcb720857a0fbd9f3c8ae6a9
Mikel Garín <mikelgarin@gmail.com>
2025-08-07 00:44:33 +0200

    Styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,12 @@ import Navbar from './Navbar';
 
 export default function Header() {
   return (
-    <div className="relative bg-emerald-950 shadow-xl">
-      <Navbar />
-      <div className="absolute inset-0 z-0 bg-ranky-texture bg-repeat bg-[length:80px] opacity-10 pointer-events-none" />
+    <div className="sticky top-0 z-50">
+      <div className="relative bg-emerald-950 shadow-xl">
+        <div className="absolute inset-0 bg-ranky-texture bg-repeat bg-[length:80px] opacity-10 pointer-events-none z-0" />
+
+        <Navbar />
+      </div>
     </div>
   );
 }
