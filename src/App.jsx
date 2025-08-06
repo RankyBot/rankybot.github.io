0e4701f2fd109892128ccfad9c2b2ff42a5f1152
Mikel Garín <mikelgarin@gmail.com>
2025-08-06 12:32:20 +0200

    Added styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { AuthProvider } from './context/AuthContext';
-import Layout from './components/Layout';
 
 import Home from './pages/Home';
 import Rankings from './pages/Rankings';
@@ -14,9 +13,9 @@ export default function App() {
       <AuthProvider>
         <Router>
           <Routes>
-            <Route path="/" element={<Layout><Home /></Layout>} />
-            <Route path="/servers/:serverId/rankings" element={<Layout><Rankings /></Layout>} />
-            <Route path="/servers/:serverId/ranking/:rankingId" element={<Layout><LoLRanking /></Layout>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/servers/:serverId/rankings" element={<Rankings />} />
+            <Route path="/servers/:serverId/ranking/:rankingId" element={<LoLRanking />} />
           </Routes>
         </Router>
       </AuthProvider>
