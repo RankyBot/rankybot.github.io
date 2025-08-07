dc9705f01df16c2597415ab30b06e0ca4c4b16e5
Mikel Garín <mikelgarin@gmail.com>
2025-08-07 10:57:27 +0200

    Styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,7 @@ import Navbar from './Navbar';
 export default function Header() {
   return (
     <div className="sticky top-0 z-50">
-      <div className="relative bg-black shadow-xl overflow-hidden border-b border-white transition-shadow duration-300 hover:shadow-[0_4px_8px_0_rgba(255,255,255,0.6)]">
-        <div className="absolute top-0 left-0 w-12 h-full bg-gradient-to-r from-emerald-950 via-transparent to-transparent z-10 pointer-events-none" />
-        <div className="absolute top-0 right-0 w-12 h-full bg-gradient-to-l from-emerald-950 via-transparent to-transparent z-10 pointer-events-none" />
+      <div className="relative bg-black shadow-xl overflow-hidden border-b border-white shadow-[0_4px_8px_0_rgba(255,255,255,0.6)]">
         <Navbar />
       </div>
     </div>
