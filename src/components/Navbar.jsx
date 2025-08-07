c1d3b13e307029c8b89b9bcc28b35e0e80549dc3
Mikel Garín <mikelgarin@gmail.com>
2025-08-07 09:44:42 +0200

    Styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,14 @@ export default function Navbar() {
     <nav className="px-6 md:container md:mx-auto md:px-8 py-4 flex items-center justify-between relative z-10">
       <Link
         to="/"
-        className="group flex items-center gap-2 md:gap-3 relative transition-shadow duration-300 hover:shadow-[0_0_12px_2px_rgba(255,255,255,0.5)] rounded-lg">
+        className="group flex items-center gap-2 md:gap-3"
+      >
         <img
           src="/ranky-logo.png"
           alt="Ranky logo"
-          className="w-8 h-8 md:w-10 md:h-10 rounded-full"
+          className="w-8 h-8 md:w-10 md:h-10 rounded-full transition-shadow duration-300 group-hover:shadow-[0_0_8px_2px_rgba(255,255,255,0.5)]"
         />
-        <span className="text-xl md:text-3xl font-black tracking-tight bg-gradient-to-r from-yellow-400 via-yellow-300 to-gray-300 text-transparent bg-clip-text">
+        <span className="text-xl md:text-3xl font-black tracking-tight bg-gradient-to-r from-yellow-400 via-yellow-300 to-gray-300 text-transparent bg-clip-text transition-shadow duration-300 group-hover:shadow-[0_0_8px_2px_rgba(255,255,255,0.5)]">
           Ranky
         </span>
       </Link>
@@ -28,7 +29,7 @@ export default function Navbar() {
           href="https://discord.com/oauth2/authorize?client_id=1005188427634966629&permissions=2952866832&scope=bot"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-gradient-to-r from-yellow-200 via-yellow-100 to-yellow-50 text-amber-900 font-semibold px-3 md:px-4 py-2 rounded hover:brightness-110 hover:-translate-y-0.5 hover:shadow-[0_0_12px_2px_rgba(255,255,255,0.5)] transition-all duration-300 text-sm md:text-base"
+          className="bg-gradient-to-r from-yellow-200 via-yellow-100 to-yellow-50 text-amber-900 font-semibold px-3 md:px-4 py-2 rounded hover:brightness-110 hover:shadow-[0_0_12px_2px_rgba(255,255,255,0.5)] transition-all duration-400 text-sm md:text-base"
         >
           Add Ranky to your Discord server
         </a>
