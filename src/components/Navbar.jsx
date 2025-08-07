9be9e9bf07c60d2d5668d1a3e6ea9ea5911a6119
Mikel Garín <mikelgarin@gmail.com>
2025-08-07 09:55:38 +0200

    Styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,16 +7,13 @@ export default function Navbar() {
 
   return (
     <nav className="px-6 md:container md:mx-auto md:px-8 py-4 flex items-center justify-between relative z-10">
-      <Link
-        to="/"
-        className="group flex items-center gap-2 md:gap-3"
-      >
+      <Link to="/" className="group flex items-center gap-2 md:gap-3">
         <img
           src="/ranky-logo.png"
           alt="Ranky logo"
-          className="w-8 h-8 md:w-10 md:h-10 rounded-full transition-shadow duration-300 group-hover:shadow-[0_0_8px_2px_rgba(255,255,255,0.5)]"
+          className="glow-animate w-8 h-8 md:w-10 md:h-10 rounded-full"
         />
-        <span className="glow-text text-xl md:text-3xl font-black tracking-tight bg-gradient-to-r from-yellow-400 via-yellow-300 to-gray-300 text-transparent bg-clip-text">
+        <span className="glow-animate text-xl md:text-3xl font-black tracking-tight bg-gradient-to-r from-yellow-400 via-yellow-300 to-gray-300 text-transparent bg-clip-text">
           Ranky
         </span>
       </Link>
@@ -29,7 +26,7 @@ export default function Navbar() {
           href="https://discord.com/oauth2/authorize?client_id=1005188427634966629&permissions=2952866832&scope=bot"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-gradient-to-r from-yellow-200 via-yellow-100 to-yellow-50 text-amber-900 font-semibold px-3 md:px-4 py-2 rounded hover:brightness-110 hover:shadow-[0_0_12px_2px_rgba(255,255,255,0.5)] transition-all duration-400 text-sm md:text-base"
+          className="glow-animate bg-gradient-to-r from-yellow-50 via-yellow-100 to-yellow-200 text-amber-900 font-semibold px-3 md:px-4 py-2 rounded hover:brightness-110 hover:-translate-y-0.5 hover:shadow-lg transition-all duration-200 text-sm md:text-base"
         >
           Add Ranky to your Discord server
         </a>
